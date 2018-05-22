4779111511fec3759497d55b25153012294fc4c2
Xudong Liu <xudong.liu@techcul.com>
2018-05-22 10:36:11 +0800

    Initial Commit

---
+++ GLKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GLKitDemo
+//
+//  Created by techcul_iOS on 2018/5/22.
+//  Copyright © 2018年 techcul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
